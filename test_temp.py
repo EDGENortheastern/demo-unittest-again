1bdaab21d8d9b3470c011d9e181dbb874b9ecc3b
Katia <katia.punter@storyshareplatform.com>
2025-04-08 14:37:15 +0100

    more tests

--- test_temp.py
+++ test_temp.py
@@ -9,6 +9,12 @@ class TestTemp(unittest.TestCase):
     def test_c_to_f_happy(self):
         self.assertEqual(celsius_to_far(0), 32)
         self.assertEqual(celsius_to_far(-40), float(-40))
+        self.assertEqual(celsius_to_far(100), 212)
+
+    def test_c_to_f_unhappy(self):
+        self.assertNotEqual(celsius_to_far(0), 35)
+        self.assertNotEqual(celsius_to_far(-40), float(40))
+        self.assertNotEqual(celsius_to_far(100), 12)
 
 if __name__ == "__main__":
     unittest.main()
