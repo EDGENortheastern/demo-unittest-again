d752b0e966a79359a5ff6dfb99477f572b1d5f67
Katia <katia.punter@storyshareplatform.com>
2025-04-08 14:30:15 +0100

    second failing test

--- test_temp.py
+++ test_temp.py
@@ -2,8 +2,12 @@ import unittest
 from temp import celsius_to_far
 
 class TestTemp(unittest.TestCase):
+
     def test_tests_work(self):
         self.assertEqual(2+2, 4)
+
+    def test_c_to_f_happy(self):
+        self.assertEqual(celsius_to_far(0), 32)
 
 if __name__ == "__main__":
     unittest.main()
